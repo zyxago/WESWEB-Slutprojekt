c9d09cedfcb84adfaa47f3ed512f2c43a4af1e61
erik hult <erik.hult123@gmail.com>
2019-05-04 10:15:49 +0200

    Jobbat med kommenterings och 'score' systemet

--- Index.php
+++ Index.php
@@ -5,12 +5,38 @@ if(!empty($_GET["intent"]) && $_GET["intent"] == "out") {
 	unset($_SESSION["user"]);
 	header("Location: index.php");
 }
+if(!empty($_POST["+1"])) {
+	$sql = "UPDATE article
+	SET points=points+1
+	WHERE ";//FIXA så att man får tag på rätt articleID.
+}
+if(!empty($_POST["-1"])) {
+	$sql = "UPDATE article
+	SET points=points-1
+	WHERE ";//FIXA så att man får tag på rätt articleID.
+	if($conn->query($sql) === TRUE) {
+		echo "YAY";
+	}
+}
 if(!empty($_POST["comment"])) {
-	//lägg till kommentar på artikel
+	$user = $_SESSION["user"];
+	$articleID = $_GET["article"];
+	$comment = $_POST["comment"];
+	if($sql = mysqli_prepare($conn, "INSERT INTO comments(articleID, user, comment)
+			VALUES (?, ?, ?)")) {
+				mysqli_stmt_bind_param($sql, "sss", $articleID, $user, $comment);//FIXA så att man får tag på rätt articleID.
+				if(mysqli_stmt_execute($sql)) {
+					echo "Comment submittet";
+				}
+				else {
+					echo "Error commenting";
+				}
+				mysqli_stmt_close($sql);
+	}
 }
 $n = 0;
-$articles = array(array("text"), array("score"), array("id"));
-$comments = array(array("comment"), array("articleID"));
+$articles = array(array("text"), array("score"), array("id"), array("userID"));
+$comments = array(array("comment"), array("articleID"), array("user"));
 $sql = "SELECT *
 		FROM article";
 $result = $conn->query($sql);
@@ -19,6 +45,7 @@ if (mysqli_num_rows($result) > 0) {
 		$articles["text"][$n] = $row["articleText"];
 		$articles["score"][$n] = $row["points"];
 		$articles["id"][$n] = $row["ID"];
+		$articles["userID"][$n] = $row["userID"];
 		$n++;
 	}
 }
@@ -30,7 +57,53 @@ if(mysqli_num_rows($result) > 0) {
 	while($row = mysqli_fetch_assoc($result)) {
 		$comments["comment"][$m] = $row["comment"];
 		$comments["articleID"][$m] = $row["articleID"];
+		$comments["user"][$m] = $row["user"];
 		$m++;
+	}
+}
+function GetUsername($userID, $conn) {
+	$sql = "SELECT username
+	FROM users
+	WHERE $userID = ID";
+	$result = $conn->query($sql);
+	if(mysqli_num_rows($result)) {
+		while($row = mysqli_fetch_assoc($result)) {
+			echo "{$row["username"]}";
+		}
+	}
+	else {
+		echo $conn->error;
+	}
+}
+function WriteArticle($n, $m, $articles, $comments, $conn) {
+	for($i = 0; $i < $n; $i++) {
+		echo "<article>
+			<p>{$articles["text"][$i]}<br><br>";
+		GetUsername($articles["userID"][$i], $conn);
+		echo "</p>";
+		echo $i;
+		if(isset($_SESSION["user"])) {
+			echo "<form action='Index.php?article=FIX' method='post'>
+			<input type='text' name='comment'>
+			<input type='submit' value='Kommentera'>
+			</from>";
+		}
+		echo "<p>Score: {$articles["score"][$i]}";
+		if(isset($_SESSION["user"])) {
+			echo "<form action='Index.php' method='post'>
+			<input type='submit' value='+1'>
+			<input type='submit' value='-1'>
+			</form></p>";
+		}
+		else {
+			echo "</p>";
+		}
+			for($j = 0; $j < $m; $j++) {
+				if($articles["id"][$i] == $comments["articleID"][$j]) {
+					echo "<p>{$comments["user"][$j]}: {$comments["comment"][$j]}</p>";
+				}
+			}
+		echo "</article>";
 	}
 }
 ?>
@@ -50,21 +123,7 @@ if(mysqli_num_rows($result) > 0) {
 		?>
 		<main>
 			<?php
-				for($i = 0; $i < $n; $i++) {
-					echo "<article>
-						<p>{$articles["text"][$i]}</p>";
-					echo '<form action="Index.php?action=comment" method="post">
-						<input type="text" name="comment">
-						<input type="submit" value="Kommentera">
-						</from>';
-					echo "<p>Score: {$articles["score"][$i]}</p>";
-						for($j = 0; $j < $m; $j++) {
-							if($articles["id"][$i] == $comments["articleID"][$j]) {
-								echo "<p>{$comments["comment"][$j]}</p>";
-							}
-						}
-					echo "</article>";
-				}
+				WriteArticle($n, $m, $articles, $comments, $conn);
 			?>
 		</main>
 		
--- databasStruktur/slutprojekt.sql
+++ databasStruktur/slutprojekt.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2019 at 12:18 PM
--- Server version: 10.1.35-MariaDB
--- PHP Version: 7.2.9
+-- Generation Time: May 04, 2019 at 10:13 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,7 +32,7 @@ CREATE TABLE `article` (
   `ID` int(255) UNSIGNED NOT NULL,
   `userID` int(255) UNSIGNED NOT NULL,
   `articleText` varchar(10000) COLLATE utf8_swedish_ci NOT NULL,
-  `points` int(255) NOT NULL
+  `points` int(255) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
 --
@@ -40,10 +40,9 @@ CREATE TABLE `article` (
 --
 
 INSERT INTO `article` (`ID`, `userID`, `articleText`, `points`) VALUES
-(1, 1, 'HEJ HEJ HEJ HEJ!!!', 0),
-(2, 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non ullamcorper ipsum. Praesent dictum odio non nunc ullamcorper, et commodo turpis sodales. Morbi sed diam ut purus auctor congue lacinia vel quam. Morbi aliquet, lectus vel placerat egestas, eros eros feugiat nisi, non condimentum arcu nisi in est. Vestibulum congue leo at molestie placerat. Maecenas scelerisque dui eros, ac tristique lectus consectetur at. Duis luctus nisl imperdiet, tincidunt mi viverra, semper massa. Vivamus lobortis posuere est, quis euismod massa gravida sit amet. Cras egestas justo tortor, at pharetra felis tristique vel. Donec tempor massa quis massa fringilla vestibulum. Proin commodo dui vel augue auctor elementum. Vestibulum ac purus id sapien facilisis ullamcorper. In dignissim dictum tortor et egestas. Vestibulum non lectus sit amet nisl molestie fringilla. Nulla tincidunt neque dolor, ut aliquam lectus luctus sed.\r\n\r\nSed faucibus, mi at pulvinar pellentesque, magna mauris rutrum lectus, vitae sodales nulla ipsum vitae diam. Pellentesque nisl velit, gravida ut diam malesuada, porta tristique sem. Praesent accumsan nunc id libero accumsan, vitae imperdiet metus fringilla. Sed vehicula, leo non gravida congue, quam est molestie tellus, at cursus est ante ac velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque quis maximus metus. Nulla dapibus nisi nec augue pretium mollis. Nunc vel magna ac nulla varius ultrices. Aliquam molestie, metus nec accumsan tincidunt, mauris lectus scelerisque metus, at ornare nisi urna nec neque. Morbi sed tempus erat. Nunc nec dui faucibus ligula ullamcorper semper ut nec ex. Integer quis ex non tellus hendrerit bibendum vitae eu mi. Aliquam purus urna, viverra vel dignissim sed, tincidunt nec metus. Sed diam orci, dictum ac rutrum sed, dignissim vitae tellus.\r\n\r\nFusce tempor id odio ut ultricies. Praesent vel nibh posuere lacus hendrerit ultrices id quis dolor. Mauris auctor lacinia porta. Nullam aliquet rhoncus leo. Etiam sodales purus eget arcu condimentum lobortis. Sed nec auctor ipsum. Nunc malesuada nunc at nisl congue, eget pharetra libero efficitur.\r\n\r\nInteger aliquam aliquam eros, quis venenatis ipsum pulvinar non. Mauris sit amet vulputate ante. Pellentesque sem massa, efficitur ac lectus rhoncus, finibus porta dolor. Ut elementum facilisis mi vitae semper. Aliquam in placerat risus. Nulla rhoncus nibh vel nibh pretium, ac lobortis erat maximus. Morbi malesuada nunc tortor, et varius dui luctus nec. Etiam vitae ex non quam placerat gravida. Aenean pellentesque ac odio vitae interdum. Vestibulum id augue nec tellus auctor fringilla nec in eros.\r\n\r\nNulla elementum magna ullamcorper, molestie justo non, laoreet neque. Nam id ante non tellus suscipit imperdiet sit amet a sem. Suspendisse euismod fermentum eros, sit amet pulvinar nulla. Praesent nec ipsum vel purus accumsan convallis. Aenean eget tristique eros. Quisque lacinia, ipsum finibus laoreet bibendum, lectus neque vestibulum urna, eget euismod tortor erat tristique tortor. Nulla ut sodales ante. In a ligula eget lacus bibendum interdum sed in odio. Donec convallis placerat enim, at ultricies arcu finibus venenatis. Aliquam eget tortor et risus pretium placerat eu at purus. Nullam magna mi, dictum accumsan enim ac, tincidunt suscipit mauris. Nam fermentum consequat aliquam. Pellentesque sodales est ut nunc accumsan, in commodo arcu pharetra. Pellentesque eu mauris pellentesque, lacinia risus ac, consectetur turpis. Curabitur feugiat nibh cursus, facilisis nulla in, feugiat nisl. Quisque egestas leo urna, quis interdum quam efficitur ut.', 0),
-(3, 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non ullamcorper ipsum. Praesent dictum odio non nunc ullamcorper, et commodo turpis sodales. Morbi sed diam ut purus auctor congue lacinia vel quam. Morbi aliquet, lectus vel placerat egestas, eros eros feugiat nisi, non condimentum arcu nisi in est. Vestibulum congue leo at molestie placerat. Maecenas scelerisque dui eros, ac tristique lectus consectetur at. Duis luctus nisl imperdiet, tincidunt mi viverra, semper massa. Vivamus lobortis posuere est, quis euismod massa gravida sit amet. Cras egestas justo tortor, at pharetra felis tristique vel. Donec tempor massa quis massa fringilla vestibulum. Proin commodo dui vel augue auctor elementum. Vestibulum ac purus id sapien facilisis ullamcorper. In dignissim dictum tortor et egestas. Vestibulum non lectus sit amet nisl molestie fringilla. Nulla tincidunt neque dolor, ut aliquam lectus luctus sed.\r\n\r\nSed faucibus, mi at pulvinar pellentesque, magna mauris rutrum lectus, vitae sodales nulla ipsum vitae diam. Pellentesque nisl velit, gravida ut diam malesuada, porta tristique sem. Praesent accumsan nunc id libero accumsan, vitae imperdiet metus fringilla. Sed vehicula, leo non gravida congue, quam est molestie tellus, at cursus est ante ac velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque quis maximus metus. Nulla dapibus nisi nec augue pretium mollis. Nunc vel magna ac nulla varius ultrices. Aliquam molestie, metus nec accumsan tincidunt, mauris lectus scelerisque metus, at ornare nisi urna nec neque. Morbi sed tempus erat. Nunc nec dui faucibus ligula ullamcorper semper ut nec ex. Integer quis ex non tellus hendrerit bibendum vitae eu mi. Aliquam purus urna, viverra vel dignissim sed, tincidunt nec metus. Sed diam orci, dictum ac rutrum sed, dignissim vitae tellus.\r\n\r\nFusce tempor id odio ut ultricies. Praesent vel nibh posuere lacus hendrerit ultrices id quis dolor. Mauris auctor lacinia porta. Nullam aliquet rhoncus leo. Etiam sodales purus eget arcu condimentum lobortis. Sed nec auctor ipsum. Nunc malesuada nunc at nisl congue, eget pharetra libero efficitur.\r\n\r\nInteger aliquam aliquam eros, quis venenatis ipsum pulvinar non. Mauris sit amet vulputate ante. Pellentesque sem massa, efficitur ac lectus rhoncus, finibus porta dolor. Ut elementum facilisis mi vitae semper. Aliquam in placerat risus. Nulla rhoncus nibh vel nibh pretium, ac lobortis erat maximus. Morbi malesuada nunc tortor, et varius dui luctus nec. Etiam vitae ex non quam placerat gravida. Aenean pellentesque ac odio vitae interdum. Vestibulum id augue nec tellus auctor fringilla nec in eros.\r\n\r\nNulla elementum magna ullamcorper, molestie justo non, laoreet neque. Nam id ante non tellus suscipit imperdiet sit amet a sem. Suspendisse euismod fermentum eros, sit amet pulvinar nulla. Praesent nec ipsum vel purus accumsan convallis. Aenean eget tristique eros. Quisque lacinia, ipsum finibus laoreet bibendum, lectus neque vestibulum urna, eget euismod tortor erat tristique tortor. Nulla ut sodales ante. In a ligula eget lacus bibendum interdum sed in odio. Donec convallis placerat enim, at ultricies arcu finibus venenatis. Aliquam eget tortor et risus pretium placerat eu at purus. Nullam magna mi, dictum accumsan enim ac, tincidunt suscipit mauris. Nam fermentum consequat aliquam. Pellentesque sodales est ut nunc accumsan, in commodo arcu pharetra. Pellentesque eu mauris pellentesque, lacinia risus ac, consectetur turpis. Curabitur feugiat nibh cursus, facilisis nulla in, feugiat nisl. Quisque egestas leo urna, quis interdum quam efficitur ut.', 0),
-(4, 1, '<h1>Lorem</h1> ipsum dolor sit amet, consectetur adipiscing elit. Sed non ullamcorper ipsum. Praesent dictum odio non nunc ullamcorper, et commodo turpis sodales. Morbi sed diam ut purus auctor congue lacinia vel quam. Morbi aliquet, lectus vel placerat egestas, eros eros feugiat nisi, non condimentum arcu nisi in est. Vestibulum congue leo at molestie placerat. Maecenas scelerisque dui eros, ac tristique lectus consectetur at. Duis luctus nisl imperdiet, tincidunt mi viverra, semper massa. Vivamus lobortis posuere est, quis euismod massa gravida sit amet. Cras egestas justo tortor, at pharetra felis tristique vel. Donec tempor massa quis massa fringilla vestibulum. Proin commodo dui vel augue auctor elementum. Vestibulum ac purus id sapien facilisis ullamcorper. In dignissim dictum tortor et egestas. Vestibulum non lectus sit amet nisl molestie fringilla. Nulla tincidunt neque dolor, ut aliquam lectus luctus sed.\r\n\r\nSed faucibus, mi at pulvinar pellentesque, magna mauris rutrum lectus, vitae sodales nulla ipsum vitae diam. Pellentesque nisl velit, gravida ut diam malesuada, porta tristique sem. Praesent accumsan nunc id libero accumsan, vitae imperdiet metus fringilla. Sed vehicula, leo non gravida congue, quam est molestie tellus, at cursus est ante ac velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque quis maximus metus. Nulla dapibus nisi nec augue pretium mollis. Nunc vel magna ac nulla varius ultrices. Aliquam molestie, metus nec accumsan tincidunt, mauris lectus scelerisque metus, at ornare nisi urna nec neque. Morbi sed tempus erat. Nunc nec dui faucibus ligula ullamcorper semper ut nec ex. Integer quis ex non tellus hendrerit bibendum vitae eu mi. Aliquam purus urna, viverra vel dignissim sed, tincidunt nec metus. Sed diam orci, dictum ac rutrum sed, dignissim vitae tellus.\r\n\r\nFusce tempor id odio ut ultricies. Praesent vel nibh posuere lacus hendrerit ultrices id quis dolor. Mauris auctor lacinia porta. Nullam aliquet rhoncus leo. Etiam sodales purus eget arcu condimentum lobortis. Sed nec auctor ipsum. Nunc malesuada nunc at nisl congue, eget pharetra libero efficitur.\r\n\r\nInteger aliquam aliquam eros, quis venenatis ipsum pulvinar non. Mauris sit amet vulputate ante. Pellentesque sem massa, efficitur ac lectus rhoncus, finibus porta dolor. Ut elementum facilisis mi vitae semper. Aliquam in placerat risus. Nulla rhoncus nibh vel nibh pretium, ac lobortis erat maximus. Morbi malesuada nunc tortor, et varius dui luctus nec. Etiam vitae ex non quam placerat gravida. Aenean pellentesque ac odio vitae interdum. Vestibulum id augue nec tellus auctor fringilla nec in eros.\r\n\r\nNulla elementum magna ullamcorper, molestie justo non, laoreet neque. Nam id ante non tellus suscipit imperdiet sit amet a sem. Suspendisse euismod fermentum eros, sit amet pulvinar nulla. Praesent nec ipsum vel purus accumsan convallis. Aenean eget tristique eros. Quisque lacinia, ipsum finibus laoreet bibendum, lectus neque vestibulum urna, eget euismod tortor erat tristique tortor. Nulla ut sodales ante. In a ligula eget lacus bibendum interdum sed in odio. Donec convallis placerat enim, at ultricies arcu finibus venenatis. Aliquam eget tortor et risus pretium placerat eu at purus. Nullam magna mi, dictum accumsan enim ac, tincidunt suscipit mauris. Nam fermentum consequat aliquam. Pellentesque sodales est ut nunc accumsan, in commodo arcu pharetra. Pellentesque eu mauris pellentesque, lacinia risus ac, consectetur turpis. Curabitur feugiat nibh cursus, facilisis nulla in, feugiat nisl. Quisque egestas leo urna, quis interdum quam efficitur ut.', 0);
+(1, 1, 'hej', 0),
+(2, 1, 'IT WORKES', 0),
+(3, 1, '\r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu nibh quis urna molestie bibendum sit amet in ligula. Ut id dui non dolor tincidunt venenatis ut at massa. Nulla placerat convallis nulla ut aliquet. Aliquam urna mauris, sollicitudin sed finibus nec, pellentesque non felis. Fusce consectetur neque vitae mauris tempor fringilla. Vivamus a lacus aliquam, vestibulum massa dignissim, posuere est. Vestibulum vel faucibus libero, eu porttitor felis. Suspendisse potenti. Donec pharetra posuere maximus. Nulla molestie libero risus, a sagittis tortor pretium non. Vivamus vel ipsum libero. Proin non nunc eleifend, volutpat risus non, eleifend mauris. Morbi in neque vitae quam luctus ultrices sit amet eget augue. Ut nec egestas sapien. Maecenas mollis lacus neque, eget sollicitudin nunc mollis posuere. Nunc ut velit at nunc sagittis porttitor.\r\n\r\nSed a lacus lacinia, eleifend arcu ac, tempor odio. Praesent varius, erat a aliquam scelerisque, quam augue posuere leo, eu malesuada eros purus non risus. Duis condimentum ut tortor et vehicula. Sed orci mi, tincidunt vitae dictum nec, pellentesque quis magna. Quisque varius velit at ligula mollis, non mattis sem suscipit. Ut id mi hendrerit, venenatis erat a, fringilla libero. Praesent et nibh lectus. Aenean bibendum diam ac turpis cursus, a bibendum ex tempus. Vestibulum molestie pulvinar aliquam.\r\n\r\nNunc ac dolor sed eros commodo elementum. Vivamus rhoncus urna a orci tincidunt, vel posuere lorem ullamcorper. Mauris ut odio commodo, molestie ligula maximus, euismod ligula. Etiam tincidunt vestibulum accumsan. Fusce vitae pellentesque nisi, nec finibus diam. Proin ultrices felis felis, sit amet ultrices enim dignissim nec. Duis ullamcorper tellus non nisl tincidunt consectetur. Nunc blandit egestas massa, in bibendum mauris sollicitudin sollicitudin.\r\n\r\nNam fermentum ut nibh a accumsan. Vestibulum tempor tristique tortor, fringilla tempor arcu maximus eu. Nam gravida quis leo sit amet pellentesque. In ut velit lobortis magna iaculis tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan eget urna posuere interdum. In magna sem, euismod nec est ac, convallis bibendum massa. Vivamus in auctor urna, eget facilisis enim. Sed eget tempus diam.\r\n\r\nUt vel aliquet nibh. In ultrices urna eu lobortis posuere. Nullam ultricies ut justo id consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dapibus erat ut massa pharetra, sit amet luctus dui ornare. Morbi varius nisl justo, sit amet lobortis massa porttitor vitae. Praesent porttitor urna eget gravida tincidunt. Curabitur pretium ultricies commodo. Aliquam fringilla velit orci. ', 0);
 
 -- --------------------------------------------------------
 
@@ -54,7 +53,7 @@ INSERT INTO `article` (`ID`, `userID`, `articleText`, `points`) VALUES
 CREATE TABLE `comments` (
   `ID` int(255) UNSIGNED NOT NULL,
   `articleID` int(255) UNSIGNED NOT NULL,
-  `userID` int(255) UNSIGNED NOT NULL,
+  `user` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
   `comment` varchar(5000) COLLATE utf8_swedish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
@@ -62,8 +61,8 @@ CREATE TABLE `comments` (
 -- Dumping data for table `comments`
 --
 
-INSERT INTO `comments` (`ID`, `articleID`, `userID`, `comment`) VALUES
-(1, 4, 1, 'WHAAAAAA IT WOKRS!!!');
+INSERT INTO `comments` (`ID`, `articleID`, `user`, `comment`) VALUES
+(1, 1, 'erik', 'a');
 
 -- --------------------------------------------------------
 
@@ -83,7 +82,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`ID`, `username`, `email`, `password`) VALUES
-(1, 'Erik', 'erik@com', '$2y$10$aN2tkOp5eimF97Km4JXI1uNMPmcqoRhl4DZ5j3qUinqXri9dxSwcm');
+(1, 'erik', 'erik.hult676@gmail.com', '$2y$10$K5BkcR5YyaO7qhcB3UYqYuIrnD30eGGp3nvVExlqml5A2cUosDAaa');
 
 --
 -- Indexes for dumped tables
@@ -102,14 +101,14 @@ ALTER TABLE `article`
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `articleID` (`articleID`),
-  ADD KEY `userID` (`userID`);
+  ADD KEY `user` (`user`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`username`) USING BTREE,
-  ADD KEY `ID` (`ID`) USING BTREE;
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -119,19 +118,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `article`
 --
 ALTER TABLE `article`
-  MODIFY `ID` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `ID` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -147,8 +146,8 @@ ALTER TABLE `article`
 -- Constraints for table `comments`
 --
 ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`),
-  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`articleID`) REFERENCES `article` (`ID`);
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`articleID`) REFERENCES `article` (`ID`),
+  ADD CONSTRAINT `comments_ibfk_3` FOREIGN KEY (`user`) REFERENCES `users` (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
